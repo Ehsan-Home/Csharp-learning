9062a6c6d00b6352f7837bd33c1e181056b7f0b7
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-20 11:50:42 -0800

    DataTypes
    different datatypes are defined

--- DataTypes/Program.cs
+++ DataTypes/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace DataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10;
+            float b = 30.3f;
+            decimal d = 10.3m;
+            bool bo = false;
+
+            string str = "a string";
+            char ch = 'c';
+            int[] arrayStr = new int[5];
+            char[] arrayCh = { 'e', 'h', 's', 'a', 'n' };
+
+            Console.WriteLine("{0},{1},{2}", a, b, d);
+            Console.WriteLine(d.ToString());
+
+        }
+    }
+}
