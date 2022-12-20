6a60e67bd13fe482b1c5ab027fc0640230cfe14e
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-20 12:29:47 -0800

    Operators
    Played with ??, ??= , and basic operators

---
+++ Operators/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 5;
+
+            Console.WriteLine("a/b:" + a / b);
+            Console.WriteLine("a%b:" + a % b);
+            Console.WriteLine("a*b:" + a * b);
+
+            string str1 = "hello", str2 = " world";
+
+            Console.WriteLine(str1 + str2);
+
+            string str3 = null;
+            // Similar to swift
+            Console.WriteLine(str3 ?? "no value");
+
+            // if (str3 == null)
+            // {
+            //     str3 = "No value";
+            // }
+            str3 ??= "Assigned value";
+            Console.WriteLine(str3);
+        }
+    }
+}
