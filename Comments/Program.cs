97fae3fd7eae7bb2844ebb766dec83165b0cd270
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-20 12:42:07 -0800

    Comments
    played with commenting and generating comment xml file

---
+++ Comments/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Comments
+{
+    class Program
+    {
+        /*
+        Comment line 1
+        Comment line 2
+        Comment line 3
+        Comment line 4
+        */
+
+        /// <summary>This is main function</summary>
+        /// <param name="args">args input as string[]</param>
+        /// <returns>no return value</returns>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Comments");
+        }
+    }
+}
