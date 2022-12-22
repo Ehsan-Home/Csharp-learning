3f5fd2f617f92f2f4a0178c64e13bc52964c2f11
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-21 17:06:24 -0800

    Exceptions
    worked with try, catch, finally,and custom exception

---
+++ Exceptions/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            string[] validNames = { "ehsan", "arman", "peyman", "sasan" };
+            try
+            {
+                if (validNames.Contains(name))
+                {
+                    Console.WriteLine("name is correct");
+                }
+                else
+                {
+                    throw new ArgumentOutOfRangeException("name", "name is not valid");
+                }
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Best place for cleanup.");
+            }
+        }
+    }
+}
