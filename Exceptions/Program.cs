f64e396874d8e2017442a4eeed72fb0b5aa95c03
ehsanghasaei <ehsan.ghasaei@gmail.com>
2022-12-22 19:10:13 -0800

    Strings
    Playing with strings

--- Exceptions/Program.cs
+++ Exceptions/Program.cs
@@ -6,6 +6,23 @@ namespace Exceptions
     {
         static void Main(string[] args)
         {
+            string str = "hi there";
+            string str2 = str.Remove(4, 1);
+            Console.WriteLine(str2);
+            // int x = 100, y = 0;
+            // try
+            // {
+            //     Console.WriteLine(x / y);
+            // }
+            // catch (DivideByZeroException e)
+            // {
+            //     Console.WriteLine(e.Message);
+            // }
+            // finally
+            // {
+            //     Console.WriteLine("this is executed each time.");
+            // }
+
             string name = Console.ReadLine();
             string[] validNames = { "ehsan", "arman", "peyman", "sasan" };
             try
